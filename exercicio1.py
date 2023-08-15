fff71435de8991fb9662b71bd0378105c0c67249
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2023-08-15 13:49:39 -0300

    feat exercicios

--- exercicio1.py
+++ exercicio1.py
@@ -1,26 +1,24 @@
 #  Se achar necessario, faça import de outras bibliotecas
 
-
-
-
-
 # Crie a função que será avaliada no exercício aqui
 
+def conta_palavras_unicas(frase):
+    # Dividir a frase em palavras
+    palavras = frase.split()
 
+    # Criar um conjunto vazio para armazenar palavras únicas
+    palavras_unicas = set()
 
+    # Iterar pelas palavras e adicioná-las ao conjunto de palavras únicas
+    for palavra in palavras:
+        palavras_unicas.add(palavra)
 
+    # Retornar a contagem de palavras únicas
+    return len(palavras_unicas)
 
-
+# Uso da função:
+frase = "Teste oi eae oi oi ola"
+quantidade_palavras_unicas = conta_palavras_unicas(frase)
+print(quantidade_palavras_unicas)
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
--- exercicio2.py
+++ exercicio2.py
@@ -1,26 +1,26 @@
 #  Se achar necessario, faça import de outras bibliotecas
 
-
-
-
-
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(str1, str2):
+    # Removendo espaços e convertendo para letras minúsculas para facilitar a comparação
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+    
+    # Convertendo as strings para listas para permitir a ordenação
+    str1_list = list(str1)
+    str2_list = list(str2)
+    
+    # Ordenando as listas
+    str1_list.sort()
+    str2_list.sort()
+    
+    # Comparando as listas ordenadas
+    return str1_list == str2_list
 
-
-
-
-
+# Uso da função
+print(is_anagram("Amor", "Roma"))  
+print(is_anagram("Pedra", "Padre")) 
+print(is_anagram("Perda", "Pedra"))  
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
